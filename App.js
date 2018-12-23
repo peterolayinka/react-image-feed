17be57d7cc5eb6222cf4a58367c2c73308aee203
Peter Olayinka <peterolayinka97@gmail.com>
2018-12-24 00:52:16 +0100

    concluded comment section

--- App.js
+++ App.js
@@ -1,30 +1,68 @@
 import React from 'react';
 import {Constants} from 'expo' 
-import { StyleSheet, Text, View } from 'react-native';
-import CardList from './component/CardList';
+import { StyleSheet, Platform, View, Modal } from 'react-native';
+import Feed from './screen/Feed';
+import Comments from './screen/Comments';
+// import CommentInput from './component/CommentInput';
 
 
-const items = [
-  {id: '0', author: 'Bob Ross'},
-  {id: '1', author: 'Chuck Norris'},
-]
 export default class App extends React.Component {
+  state = {
+    commentsForItem: {},
+    showModal: false,
+    selectedItemId: null,
+  }
+
+  openCommentScreen = id => {
+    this.setState({
+      showModal: true,
+      selectedItemId: id,
+    })
+  }
+
+  closeCommentScreen = () => {
+    this.setState({
+      showModal: false,
+      selectedItemId: null
+    })
+  }
+
+  onSubmitComment = (text) => {
+    const { selectedItemId, commentsForItem } = this.state;
+    const comments = commentsForItem[selectedItemId] || [];
+
+    const updated = {
+      ...commentsForItem,
+      [selectedItemId]: [...comments, text],
+    }
+    this.setState({commentsForItem: updated});
+  }
+
   render() {
+    const {commentsForItem, showModal, selectedItemId} = this.state;
     return (
       <View style={styles.container}>
-        <CardList
-          items={items}
-        />
-        {/* <Card
-          fullname={'First Last'}
-          linkText={'Comments'}
-          onPressLinkText={()=>{console.log('Pressed Link!')}}
-          image={{uri: 'https://unsplash.it/600/600'}}
-        /> */}
+        <Feed style={styles.feed} 
+          commentsForItem={commentsForItem}
+          onPressComments={this.openCommentScreen} />
+        <Modal 
+          visible={showModal}
+          animationType="slide"
+          onRequestClose={this.closeCommentScreen} >
+          <Comments 
+            style={styles.comments}
+            comments={commentsForItem[selectedItemId] || []}
+            onClose={this.closeCommentScreen}
+            onSubmitComment={this.onSubmitComment}
+          />
+        </Modal>
       </View>
     );
   }
 }
+
+const platformVersionIOS = Platform.OS === 'ios' ? parseInt(Platform.Version, 10) : Platform.Version;
+const platformVersionAndroid = Platform.OS === 'android' ? parseInt(Platform.Version, 7) : Platform.Version;
 
 const styles = StyleSheet.create({
   container: {
@@ -32,4 +70,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     marginTop: Constants.statusBarHeight,
   },
+  feed: {
+    flex: 1,
+    marginTop: platformVersionAndroid < 7 || platformVersionIOS < 11 
+      ? Constants.statusBarHeight 
+      : 0,
+  },
+  comments: {
+    flex: 1,
+    marginTop: platformVersionAndroid < 7 || (Platform.OS === 'ios' && platformVersionIOS < 11) 
+      ? Constants.statusBarHeight: 0,
+  }
 });
--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 export const fetchImages = async () => {
 	const response = await fetch('https://unsplash.it/list');
 	const images = await response.json();
-
+	console.log("fetching images")
 	return images;
 };
 
